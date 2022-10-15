5de8d764d7affde48111512727ccc530912cbf13
Ifeoluwa Gabriel <ifeoluwagabe@gmail.com>
2022-10-15 16:45:30 +0100

    new commit

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 let bg = document.querySelector('.bg-img')
 let text = document.querySelector('.text')
 let btn = document.querySelector('.btn')
@@ -10,9 +9,9 @@ function scale (number, inMin, inMax, outMin, outMax) {
 }
 
 btn.addEventListener('click', unblur)
-let loader = 0;
 
 function unblur(){
+    let loader = 0;
     let int = setInterval(load, 30)
 
     function load() {
@@ -26,5 +25,24 @@ function unblur(){
         btn.style.opacity = scale(loader, 0, 100, 1, 0);
         rnm.style.opacity = scale(loader, 0, 100, 0, 1);
         bg.style.filter = `blur(${scale(loader, 0, 100, 30, 0)}px)`
+    }
+}
+
+rnm.addEventListener('click', blur)
+
+function blur(){
+    let loader2 = 100
+    let int2 = setInterval(reblur, 30)
+
+    function reblur(){
+        loader2--
+        if(loader2 < 1){
+            clearInterval(int2)
+        }
+        text.innerHTML = `${loader2}%`
+        text.style.opacity = scale(loader2, 100, 0, 0, 1)
+        bg.style.filter = `blur(${scale(loader2, 100, 0, 0, 30)}px)`
+        rnm.style.opacity = scale(loader2, 100, 0, 1, 0)
+        btn.style.opacity = scale(loader2, 100, 0, 0, 1);
     }
 }
